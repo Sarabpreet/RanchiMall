281b87be0a8177865c82a2603f8bbf2c65055b85
Sarabpreet S <Sarabpreetsingh23@gmail.com>
2017-09-23 19:55:21 +0530

    changes++

---
+++ frontend/gulpfile.js
@@ -0,0 +1,33 @@
+
+
+var gulp = require('gulp');
+var less = require('gulp-less'); 
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+ var util = require('gulp-util');
+ 
+/* Task to compile less */
+gulp.task('compile-less', function() {
+// console.log("comling less");  
+  gulp.src('./less/custom.less').pipe(less().on('error', util.log)).pipe(gulp.dest('./css/'));
+}); 
+/* Task to watch less changes */
+gulp.task('watch-less', function() { 
+console.log("watching less"); 
+  gulp.watch('./less/*.less' , ['compile-less']);
+});
+ 
+gulp.task('serve', function () {
+ 	// console.log("i am started");
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    }); 
+    gulp.watch("./less/*.less").on("change", reload);
+    gulp.watch("*.html").on("change", reload);
+});
+ 
+/* Task when running `gulp` from terminal */
+gulp.task('default', ['watch-less', 'serve']);
