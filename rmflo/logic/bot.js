b70270903aa5ac9012c98231319653ca048de65c
Sarabpreet S <Sarabpreetsingh23@gmail.com>
2019-02-08 18:04:35 +0530

    Changes

---
+++ rmflo/logic/bot.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) =>res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+// PULSE 
+
+var CoinMarketCap = require("node-coinmarketcap");
+var coinmarketcap = new CoinMarketCap();
+coinmarketcap.get("flo", coin => {
+console.log(coin); // Prints the price in USD of BTC at the moment.
+  });
