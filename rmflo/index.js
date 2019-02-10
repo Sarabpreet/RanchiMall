cef1b0978c5f8e1984d020adeddf411f1ef08684
Sarabpreet S <Sarabpreetsingh23@gmail.com>
2019-02-10 16:33:37 +0530

    changes

--- rmflo/index.js
+++ rmflo/index.js
@@ -6,9 +6,34 @@ app.get('/', (req, res) =>res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 // PULSE 
-
+var CronJob = require('cron').CronJob;
 var CoinMarketCap = require("node-coinmarketcap");
 var coinmarketcap = new CoinMarketCap();
+// FIREBASECRED
+var admin = require('firebase-admin');
+var serviceAccount = require('./secret/key.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://ranchimall.firebaseio.com'
+});
+
+var db = admin.database();
+var ref = db.ref("flo");
+// var usersRef = ref.child("flo");
+
+
 coinmarketcap.get("flo", coin => {
-console.log(coin); // Prints the price in USD of BTC at the moment.
-  });
+  // console.log(coin); // Prints the price in USD of BTC at the moment.
+  ref.set(coin);
+});
+
+
+// 
+
+
+// EVERY SECOND 
+new CronJob('* * * * * *', function() {
+  // console.log('You will see this message every second');
+  
+}, null, true, 'America/Los_Angeles');
+
